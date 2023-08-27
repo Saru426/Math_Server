6406cda5ba420c5181aab6129962edae932f4a2b
Sarthak Singh <sarthaksingh426@gmail.com>
2023-08-27 14:58:21 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const app = express();
 
-// Initialize the history array
 const history = [];
 
-// Middleware to parse URL parameters
 app.use(express.urlencoded({ extended: false }));
 
-// Endpoint to handle mathematical operations
 app.get('/:operands/:operator/:operands2', (req, res) => {
   const { operands, operator, operands2 } = req.params;
   const question = `${operands}/${operator}/${operands2}`;
   const answer = calculate(operands, operator, operands2);
   
-  // Add this operation to the history
   addToHistory(question, answer);
 
-  // Send JSON response
   res.json({ question, answer });
 });
 
@@ -26,10 +21,8 @@ app.get('/:operands/:operator/:operands2/:operator2/:operands3', (req, res) => {
     const panswer = calculate(operands, operator, operands2);
     const answer = calculate(panswer, operator2, operands3);
     
-    // Add this operation to the history
     addToHistory(question, answer);
   
-    // Send JSON response
     res.json({ question, answer });
 });
 
@@ -40,20 +33,16 @@ app.get('/:operands/:operator/:operands2/:operator2/:operands3/:operator3/:opera
     const answer2 = calculate(answer1, operator2, operands3);
     const answer = calculate(answer2, operator3, operands4);
     
-    // Add this operation to the history
     addToHistory(question, answer);
   
-    // Send JSON response
     res.json({ question, answer });
 });
 
 
-// Endpoint to view the history
 app.get('/history', (req, res) => {
   res.json(history);
 });
 
-// Function to calculate mathematical operations
 function calculate(operands, operator, operands2) {
   operands = parseFloat(operands);
   operands2 = parseFloat(operands2);
@@ -67,13 +56,11 @@ function calculate(operands, operator, operands2) {
       return operands * operands2;
     case 'dividedby':
       return operands / operands2;
-    // Add more operators as needed
     default:
       return NaN;
   }
 }
 
-// Function to add an operation to the history (max 20 entries)
 function addToHistory(question, answer) {
   history.unshift({ question, answer });
   if (history.length > 20) {
